0d83bc58b757c25653133b04838692567831bfca
Juan <jperez301@mail.valenciacollege.edu>
2023-01-09 12:57:26 -0500

    Changed the way firebase reads credentials to a local file.

--- src/main/java/com/jcpdev/ModernGreenThumb/FBInitialize.java
+++ src/main/java/com/jcpdev/ModernGreenThumb/FBInitialize.java
@@ -1,9 +1,13 @@
 package com.jcpdev.ModernGreenThumb;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.stereotype.Service;
+
 import javax.annotation.PostConstruct;
+import java.io.InputStream;
+
 import static com.jcpdev.ModernGreenThumb.CONSTANTS.DATABASEURL;
 
 
@@ -11,13 +15,12 @@ import static com.jcpdev.ModernGreenThumb.CONSTANTS.DATABASEURL;
 public class FBInitialize {
 
 
-
     @PostConstruct
     public void initialize() {
         try {
-
+            InputStream serviceAccount = (FBInitialize.class.getResourceAsStream("/googlecredentials.json"));
             FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.getApplicationDefault())
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .setDatabaseUrl(DATABASEURL)
                     .build();
 
